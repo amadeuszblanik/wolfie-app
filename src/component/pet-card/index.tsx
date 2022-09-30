c881e9911bcc45f29bccceae73c4a817ce156b1f
Amadeusz Blanik <amadeusz@blanik.me>
2022-09-30 20:37:11 +0200

    Removed blinky backgrounds

--- src/component/pet-card/index.tsx
+++ src/component/pet-card/index.tsx
@@ -6,7 +6,6 @@ import { DoggoTextVariant } from "../../ui-components/text";
 import { FormattedMessage } from "react-intl";
 import { pipeAge, pipeDate } from "../../pipe";
 import { ThemePalette } from "styled-components";
-import { randomElement } from "bme-utils";
 import { Breed } from "../../types/breed.types";
 
 interface Props {
@@ -15,46 +14,45 @@ interface Props {
   image?: string;
   birthDate: Date;
   breed?: Breed;
+  background?: [ThemePalette, ThemePalette];
 }
 
-const Component: React.FunctionComponent<Props> = ({ name, birthDate, microchip, breed, image }) => {
-  const availableBackgrounds: ThemePalette[][] = [
-    ["indigo", "blue"],
-    ["red", "indigo"],
-    ["red", "purple"],
-    ["indigo", "purple"],
-    ["blue", "cyan"],
-  ];
+// const availableBackgrounds: ThemePalette[][] = [
+//   ["indigo", "blue"],
+//   ["red", "indigo"],
+//   ["red", "purple"],
+//   ["indigo", "purple"],
+//   ["blue", "cyan"],
+// ];
 
-  return (
-    <DoggoBox
-      width={BoxWidth.Full}
-      padding={{ y: SizesEnum.ExtraLarge, x: SizesEnum.ExtraLarge }}
-      background={randomElement(availableBackgrounds) ?? ["indigo", "blue"]}
-      column
-    >
-      <DoggoBox alignX={FlexAlign.SpaceBetween} padding={{ bottom: SizesEnum.Medium }}>
-        <DoggoText variant={DoggoTextVariant.LargeTitle}>{name}</DoggoText>
-        <DoggoAvatar alt="Avatar" size={SizesEnum.ExtraLarge}>
-          {image}
-        </DoggoAvatar>
-      </DoggoBox>
-      <DoggoBox width={BoxWidth.Full} column>
-        <DoggoText>
-          <FormattedMessage id="pet.age" />: {pipeAge(birthDate)}
-        </DoggoText>
-        <DoggoText>
-          <FormattedMessage id="pet.birthday" />: {pipeDate(birthDate)}
-        </DoggoText>
-        <DoggoText>
-          <FormattedMessage id="pet.microchip" />: {microchip}
-        </DoggoText>
-        <DoggoText>
-          <FormattedMessage id="pet.breed" />: {breed?.name ?? <FormattedMessage id="pet.breed.mixed" />}
-        </DoggoText>
-      </DoggoBox>
+const Component: React.FunctionComponent<Props> = ({ name, birthDate, microchip, breed, image, background }) => (
+  <DoggoBox
+    width={BoxWidth.Full}
+    padding={{ y: SizesEnum.ExtraLarge, x: SizesEnum.ExtraLarge }}
+    background={background ?? ["indigo", "blue"]}
+    column
+  >
+    <DoggoBox alignX={FlexAlign.SpaceBetween} padding={{ bottom: SizesEnum.Medium }}>
+      <DoggoText variant={DoggoTextVariant.LargeTitle}>{name}</DoggoText>
+      <DoggoAvatar alt="Avatar" size={SizesEnum.ExtraLarge}>
+        {image}
+      </DoggoAvatar>
     </DoggoBox>
-  );
-};
+    <DoggoBox width={BoxWidth.Full} column>
+      <DoggoText>
+        <FormattedMessage id="pet.age" />: {pipeAge(birthDate)}
+      </DoggoText>
+      <DoggoText>
+        <FormattedMessage id="pet.birthday" />: {pipeDate(birthDate)}
+      </DoggoText>
+      <DoggoText>
+        <FormattedMessage id="pet.microchip" />: {microchip}
+      </DoggoText>
+      <DoggoText>
+        <FormattedMessage id="pet.breed" />: {breed?.name ?? <FormattedMessage id="pet.breed.mixed" />}
+      </DoggoText>
+    </DoggoBox>
+  </DoggoBox>
+);
 
 export default Component;

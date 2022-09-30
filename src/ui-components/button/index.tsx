4fbda8fc125469604a19c361cdad31b518f29400
Amadeusz Blanik <amadeusz@blanik.me>
2022-09-30 17:19:11 +0200

    Fixed button text detection

--- src/ui-components/button/index.tsx
+++ src/ui-components/button/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import styled, { DefaultTheme } from "styled-components";
 import { darkenColorMixin, isDarkMixin, paddingMixin } from "../mixins";
 import { SizesEnum } from "../../settings/sizes";
 import Text from "../text";
+import { FormattedMessage } from "react-intl";
 
 interface StyledButtonProps {
   variant: keyof DefaultTheme["palette"];
@@ -39,15 +40,20 @@ const StyledButton = styled.button<StyledButtonProps>`
   }
 `;
 
-const Button: React.FunctionComponent<ButtonProps> = ({ children, variant, onClick, disabled }) => (
-  <StyledButton
-    variant={variant!}
-    onClick={!disabled ? onClick : () => console.warn("Button disabled")}
-    disabled={disabled}
-  >
-    {children instanceof String ? <Text noBottomMargin>{children}</Text> : children}
-  </StyledButton>
-);
+const Button: React.FunctionComponent<ButtonProps> = ({ children, variant, onClick, disabled }) => {
+  const childrenType = (children as ReactElement | null)?.type === FormattedMessage;
+  const isText = typeof children === "string" || childrenType;
+
+  return (
+    <StyledButton
+      variant={variant!}
+      onClick={!disabled ? onClick : () => console.warn("Button disabled")}
+      disabled={disabled}
+    >
+      {isText ? <Text noBottomMargin>{children}</Text> : children}
+    </StyledButton>
+  );
+};
 
 Button.defaultProps = {
   variant: "blue",

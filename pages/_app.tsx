c27eebe3f4f9c716af50d9caf0648744087b1bbd
Amadeusz Blanik <amadeusz@blanik.me>
2023-01-03 03:25:59 +0100

    Configured project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "styled-components";
+import { BmeGlobalStyle, BmeThemeProvider } from "bme-ui";
+import { theme } from "../src/settings";
+import { GlobalStyles } from "../src/components";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <BmeThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BmeGlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </BmeThemeProvider>
+  );
 }

3417cd4b93daeefa8ca94d08d650dbf62459fefa
Amadeusz Blanik <amadeusz@blanik.me>
2023-03-24 03:37:48 +0100

    Added scroll to layout auth

--- src/layouts/auth/index.tsx
+++ src/layouts/auth/index.tsx
@@ -14,10 +14,23 @@ const StyledMain = styled.main`
   display: flex;
   flex-direction: column;
   width: 100%;
-  min-height: var(--bme-vh, 100vh);
+  height: var(--bme-vh, 100vh);
 
   @media (min-width: ${({ theme }) => theme.breakpoints.desktop}px) {
     flex-direction: row;
+  }
+`;
+
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  width: 100%;
+  overflow-y: scroll;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktop}px) {
+    height: var(--bme-vh, 100vh);
   }
 `;
 
@@ -47,15 +60,12 @@ const Layout: React.FC<LayoutAppProps> = ({ title, children }) => {
           </BmeBox>
           <BmeText variant="Title2">{title}</BmeText>
         </BmeBox>
-        <BmeBox
-          direction="column"
-          alignX="center"
-          alignY="center"
-          width={isMobile ? "100%" : "50%"}
-          minHeight="var(--bme-vh, 100vh)"
-          padding="md|no"
-        >
-          <Container>{children}</Container>
+        <BmeBox alignX="center" alignY="center" width={isMobile ? "100%" : "50%"}>
+          <StyledContent>
+            <BmeBox direction="column" alignX="center" alignY="center" width="100%" margin="auto|no" padding="md|no">
+              <Container>{children}</Container>
+            </BmeBox>
+          </StyledContent>
         </BmeBox>
       </StyledMain>
     </>

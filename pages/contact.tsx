e23e6ee736cca53dff705603d9f933881b15328b
Amadeusz Blanik <amadeusz@blanik.me>
2023-08-21 18:25:56 +0200

    Added contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,10 @@
+import { LayoutLanding } from "../src/layouts";
+import { SectionContact } from "../src/sections";
+
+export default function Page() {
+  return (
+    <LayoutLanding>
+      <SectionContact />
+    </LayoutLanding>
+  );
+}

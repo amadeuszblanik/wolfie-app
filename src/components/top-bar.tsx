48e5d417d812e90fb1b14627fa666a9837c49354
Amadeusz Blanik <amadeusz@blanik.me>
2023-10-03 01:55:22 +0200

    Refactored top bar component

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,112 +1,121 @@
-import Image from "next/image";
+"use client";
 
-const Component = () => (
-  <header className="absolute inset-x-0 top-0 z-50">
-    <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <div className="flex lg:flex-1">
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Wolfie.app</span>
-          <Image src="logo.svg" alt="Wolfie.app logo" width={40} height={40} />
-        </a>
-      </div>
-      <div className="flex lg:hidden">
-        <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-        </button>
-      </div>
-      <div className="hidden lg:flex lg:gap-x-12">
-        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-          About
-        </a>
-        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-          Features
-        </a>
-        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-          Pricing
-        </a>
-        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-          Contact
-        </a>
-      </div>
-      <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-          Download <span aria-hidden="true">&rarr;</span>
-        </a>
-      </div>
-    </nav>
-    <div className="lg:hidden" role="dialog" aria-modal="true">
-      <div className="fixed inset-0 z-50"></div>
-      <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-        <div className="flex items-center justify-between">
+import Image from "next/image";
+import React, { useState } from "react";
+
+interface Item {
+  title: string;
+  link: string;
+}
+
+interface Props {
+  items: Item[];
+  cta?: Item;
+}
+
+const Component: React.FC<Props> = ({ items, cta }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Wolfie.app</span>
             <Image src="logo.svg" alt="Wolfie.app logo" width={40} height={40} />
           </a>
-          <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
-            <span className="sr-only">Close menu</span>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setIsMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
             <svg
               className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               aria-hidden="true"
             >
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
         </div>
-        <div className="mt-6 flow-root">
-          <div className="-my-6 divide-y divide-gray-500/10">
-            <div className="space-y-2 py-6">
-              <a
-                href="#"
-                className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Product
+        <div className="hidden lg:flex lg:gap-x-12">
+          {items.map(({ link, title }) => (
+            <a key={title} href={link} className="text-sm font-semibold leading-6 text-gray-900">
+              {title}
+            </a>
+          ))}
+        </div>
+        {cta && (
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a href={cta.link} className="text-sm font-semibold leading-6 text-gray-900">
+              {cta.title} <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        )}
+      </nav>
+      {isMenuOpen && (
+        <div className="lg:hidden" role="dialog" aria-modal="true">
+          <div className="fixed inset-0 z-50"></div>
+          <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Wolfie.app</span>
+                <Image src="logo.svg" alt="Wolfie.app logo" width={40} height={40} />
               </a>
-              <a
-                href="#"
-                className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              <button
+                type="button"
+                onClick={() => setIsMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
               >
-                Features
-              </a>
-              <a
-                href="#"
-                className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Marketplace
-              </a>
-              <a
-                href="#"
-                className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Company
-              </a>
+                <span className="sr-only">Close menu</span>
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
             </div>
-            <div className="py-6">
-              <a
-                href="#"
-                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-              >
-                Download
-              </a>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {items.map(({ link, title }) => (
+                    <a
+                      key={title}
+                      href={link}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {title}
+                    </a>
+                  ))}
+                </div>
+                {cta && (
+                  <div className="py-6">
+                    <a
+                      href={cta.link}
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {cta.title}
+                    </a>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </header>
-);
+      )}
+    </header>
+  );
+};
 
 export default Component;

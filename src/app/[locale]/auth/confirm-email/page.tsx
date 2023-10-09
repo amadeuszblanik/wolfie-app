ddf32cfe9a5a1ca676f9c15d0684121daf5daa83
Amadeusz Blanik <amadeusz@blanik.me>
2023-10-09 21:25:22 +0200

    Hotfix auth pages on vercel

--- src/app/[locale]/auth/confirm-email/page.tsx
+++ src/app/[locale]/auth/confirm-email/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { useTranslations } from "next-intl";
-import { useQueryConfirmEmail } from "@/query";
+import HotFix from "@/app/[locale]/auth/confirm-email/hot-fix";
 
 interface Props {
   searchParams: {
@@ -10,8 +8,6 @@ interface Props {
 }
 
 export default function Home({ searchParams: { token } }: Props) {
-  const { mutate, data, error } = useQueryConfirmEmail();
-
   const t = useTranslations("Auth.ConfirmEmail");
 
   if (!token) {
@@ -26,50 +22,5 @@ export default function Home({ searchParams: { token } }: Props) {
     );
   }
 
-  if (data) {
-    return (
-      <main>
-        <div>
-          <div className="mt-2 text-center">
-            <h1 className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">{data.message}</h1>
-          </div>
-        </div>
-      </main>
-    );
-  }
-
-  if (error) {
-    return (
-      <main>
-        <div>
-          <div className="mt-2 text-center">
-            <h1 className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">{error.message}</h1>
-          </div>
-        </div>
-      </main>
-    );
-  }
-
-  return (
-    <main>
-      <div>
-        <div className="mt-2 text-center">
-          <h1 className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">{t("title")}</h1>
-
-          <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">{t("description")}</p>
-        </div>
-      </div>
-
-      <div className="mt-5 sm:flex sm:items-center sm:justify-end">
-        <div className="sm:flex sm:items-center ">
-          <button
-            className="w-full px-4 py-2 mt-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:w-auto sm:mt-0 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-            onClick={() => mutate(token)}
-          >
-            {t("cta")}
-          </button>
-        </div>
-      </div>
-    </main>
-  );
+  return <HotFix token={token} />;
 }
---
+++ src/app/[locale]/auth/reset-password/hot-fix.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import React, { FormEventHandler, useState } from "react";
+import { useQueryResetPassword } from "@/query";
+
+interface Props {
+  token: string;
+}
+
+const Component: React.FC<Props> = ({ token }) => {
+  const t = useTranslations("Auth.ResetPassword");
+  const { mutate, data, error } = useQueryResetPassword();
+
+  const [password, setPassword] = useState("");
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    mutate({ token, password });
+  };
+
+  if (data) {
+    return (
+      <main>
+        <div>
+          <div className="mt-2 text-center">
+            <h1 className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">{data.message}</h1>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  const passwordFieldError = Object.values(
+    error?.errors?.find((cur) => cur.property === "password")?.constraints ?? [],
+  ).join(", ");
+
+  if (error && !passwordFieldError) {
+    return (
+      <main>
+        <div>
+          <div className="mt-2 text-center">
+            <h1 className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">{error.message}</h1>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <div>
+        <div className="mt-2 text-center">
+          <h1 className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">{t("title")}</h1>
+
+          <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">{t("description")}</p>
+        </div>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="w-full mt-4">
+          <input
+            className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+            type="password"
+            placeholder={t("password")}
+            aria-label={t("password")}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <p className="mt-3 text-xs text-red-400">{passwordFieldError}</p>
+        </div>
+
+        <div className="flex items-center justify-end mt-4">
+          <button
+            className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+            type="submit"
+          >
+            {t("cta")}
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default Component;
